85b6a28cbbf7943a38fdc029d8ab548861541e29
woying <loveson821@gmail.com>
2013-03-10 19:02:26 +0800

    Confirm email(Gmail&Mandrill)

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,18 @@ YAML.load(ENV['ROLES']).each do |role|
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+if user = User.find_by_email(ENV['ADMIN_EMAIL'])
+  user
+else
+  user = User.new(
+    :name => ENV['ADMIN_NAME'].dup, 
+    :email => ENV['ADMIN_EMAIL'].dup, 
+    :password => ENV['ADMIN_PASSWORD'].dup, 
+    :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
 puts 'user: ' << user.name
 user.add_role :admin
